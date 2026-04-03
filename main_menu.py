968e37dff55e780fc09fd09f6f174283bea8002e
VirgilsPath <132574864+VirgilsPath@users.noreply.github.com>
2026-04-03 12:03:38 -0500

    major update

--- main_menu.py
+++ main_menu.py
@@ -4,96 +4,176 @@ import tools_tracker
 
 data = tools_tracker.load_menu()
 
-while True:
-    print("----- Main Menu -----")
-    print("1. Show Movie List")
-    print("2. Show Tv Show List")
-    print("3. Show Music List")
-    print("4. Exit")
-    print()
+try:
+    while True:
+        print("----- Main Menu -----")
+        print("1. Show Movie List")
+        print("2. Show Tv Show List")
+        print("3. Show Music List")
+        print("4. Add Your Item")
+        print("5. Exit")
+        print()
 
-    choice = input("Choose a number to proceed: ")
+        choice = input("Choose a number to proceed: ")
 
-    if choice == "1":
-        # find list
-        movie_list = data["Media To Download"]["Movies"]
+        if choice == "1":
+            # find list
+            movie_list = data["media to download"]["movies"]
+            
+            # check if list has movies
+            if len(movie_list) > 0:
+                print("\n----- Movies -----")
+                for mov in movie_list:
+                    print(f"Name: {mov['name']} | Year: {mov['year']} | Downloaded: {mov['downloaded']}")
+            else:
+                # list is empty
+                print("\nYour movie list is empty.")
+
+            user_input = input("Type 'back' to go back or 'exit' to exit: ")
+
+            if user_input.lower() == 'back':
+                continue
+            elif user_input.lower() == 'exit':
+                print("\nGoodbye!")
+                break
         
-        # check if list has movies
-        if len(movie_list) > 0:
-            print("----- Movies -----")
-            for mov in movie_list:
-                print(f"{mov['Name']}: {mov['Year']}: {mov['Downloaded']}")
-        else:
-            # list is empty
-            print("\nYour movie list is empty.")
+        elif choice == "2":
+            # find list
+            tv_show_list = data["media to download"]["tvshows"]
+            
+            # check if list has tv shows
+            if len(tv_show_list) > 0:
+                print("\n----- Tv Shows -----")
+                for show in tv_show_list:
+                    print(f"Name: {show['name']} | Year: {show['year']} | Downloaded: {show['downloaded']}")
 
-        user_input = input("Type 'back' to go back or 'exit' to exit: ")
+                    # check if this show has the "seasons" key
+                    if "seasons" in show:
+                        # loop through sub-dictionary
+                        for s_name, s_status in show["seasons"].items():
+                            status_text = "True" if s_status else "Need"
+                            print(f"   > {s_name}: {status_text}")
 
-        if user_input.lower() == 'back':
-            continue
-        elif user_input.lower() == 'exit':
-            print("Goodbye!")
-            break
-    
-    elif choice == "2":
-        # find list
-        tv_show_list = data["Media To Download"]["Tv Shows"]
+            else:
+                # list is empty
+                print("\nYour tv show list is empty.")
+
+            user_input = input("Type 'back' to go back or 'exit' to exit: ")
+
+            if user_input.lower() == 'back':
+                continue
+            elif user_input.lower() == 'exit':
+                print("\nGoodbye!")
+                break
+
+        elif choice == "3":
+            # find list
+            music_list = data["media to download"]["music"]
+            
+            # check if list has music
+            if len(music_list) > 0:
+                print("\n----- Music -----")
+                for music in music_list:
+                    print(f"Artist: {music['name']}")
+
+                    # check for Album key
+                    if "album" in music:
+                        for a_name, a_info in music["album"].items():
+                            status_text = "True" if a_info["downloaded"] else "Need"
+                            print(f"   > {a_name} ({a_info['year']}): {status_text}")
+
+            else:
+                # list is empty
+                print("\nYour music list is empty.")
+
+            user_input = input("Type 'back' to go back or 'exit' to exit: ")
+
+            if user_input.lower() == 'back':
+                continue
+            elif user_input.lower() == 'exit':
+                print("\nGoodbye!")
+                break
         
-        # check if list has tv shows
-        if len(tv_show_list) > 0:
-            print("----- Tv Shows -----")
-            for show in tv_show_list:
-                print(f"\n{show['Name']}: {show['Year']}: {show['Downloaded']}")
+        elif choice == "4":
+            while True:
+                print("\n----- Add Item -----")
+                raw_cat = input("Category (Movies, Tv Shows, or Music) or type 'goback' to go back to menu: ").strip()
+                cat = raw_cat.lower().replace(" ", "")
 
-                # check if this show has the "Seasons" key
-                if "Seasons" in show:
-                    # loop through sub-dictionary
-                    for s_name, s_status in show["Seasons"].items():
-                        status_text = "Done" if s_status else "Needed"
-                        print(f"   > {s_name}: {status_text}")
+                if cat == "goback":
+                    print("\nReturning to Main Menu...")
+                    break
+                
+                name = ""
+                year = ""
+                is_downloaded = False
+                s_data = None
+                a_data = None
 
-        else:
-            # list is empty
-            print("\nYour tv show list is empty.")
+                if cat == "movies":
+                    name = input(f"Enter Movie Name: ")
+                    year = input(f"Enter Movie Year: ")
+                
+                    while True:
+                        is_down_raw = input("Is is downloaded? (y/n): ").lower()
+                        if is_down_raw in ['y', 'n']:
+                            is_downloaded = (is_down_raw == 'y')
+                            break
+                        print("Invalid input. Please type 'y' or 'n'.")
 
-        user_input = input("Type 'back' to go back or 'exit' to exit: ")
+                if cat == "tvshows":            
+                    name = input("Enter TV Show Name: ")
+                    year = input("Enter TV Show Year: ")
+                    s_data = {}
 
-        if user_input.lower() == 'back':
-            continue
-        elif user_input.lower() == 'exit':
-            print("Goodbye!")
+                    while True:
+                        s_num = input("Enter Season Number (e.g., Season 1) or type 'done' to finish: ").strip()
+                        if s_num.lower() == 'done':
+                            break
+                        
+                        # specific season status
+                        while True:
+                            is_down_raw = input(f"Is {s_num} downloaded? (y/n): ").lower()
+                            if is_down_raw in ['y', 'n']:
+                                s_data[s_num] = (is_down_raw == 'y')
+                                break
+                            print("Invalid input. Please type 'y' or 'n'.")
+
+                elif cat == "music":
+                    name = input("Enter Band/Artist Name: ")
+                    
+                    a_data = {}
+
+                    while True:
+                        album_name = input("Enter Album Name or type 'done' to finish: ").strip()
+                        if album_name.lower() == 'done':
+                            break
+                        
+                        album_year = input(f"Enter Album Year for '{album_name}': ")
+
+                        # ask for this specific album's status
+                        while True:
+                            is_down_raw = input(f"Is '{album_name}' downloaded? (y/n): ").lower()
+                            if is_down_raw in ['y', 'n']:
+                                a_data[album_name] = {"year": album_year, "downloaded": (is_down_raw == 'y')}
+                                break
+                            print("Invalid input. Please type 'y' or 'n'.")
+
+                    if not a_data: a_data = None
+
+
+                tools_tracker.add_item(data, cat, name, year, downloaded=is_downloaded, seasons=s_data, album=a_data)
+
+                print(f"\n--- {name} has been updated in {cat}! ---")
+                
+                data = tools_tracker.load_menu()
+                break
+
+        elif choice == "5":
+            print("\nGoodbye!")
             break
-
-    elif choice == "3":
-        # find list
-        music_list = data["Media To Download"]["Music"]
-        
-        # check if list has music
-        if len(music_list) > 0:
-            print("----- Music -----")
-            for music in music_list:
-                print(f"{music['Name']}: {music['Year']}: {music['Downloaded']}")
-
-                # check for Album key
-                if "Album" in music:
-                    for a_name, a_status in music["Albums"].items():
-                        status_text = "Done" if a_status else "Needed"
-                        print(f"   > {a_name}: {status_text}")
-
         else:
-            # list is empty
-            print("\nYour music list is empty.")
+            print("Invalid choice. Try again.")
 
-        user_input = input("Type 'back' to go back or 'exit' to exit: ")
-
-        if user_input.lower() == 'back':
-            continue
-        elif user_input.lower() == 'exit':
-            print("Goodbye!")
-            break
-    
-    elif choice == "4":
-        print("\nGoodbye!")
-        break
-    else:
-        print("Invalid choice. Try again.")
+except KeyboardInterrupt:
+    print("\n\nForced exit detected. Saving (if needed) and shutting down. Goodbye!")
--- tools_tracker.py
+++ tools_tracker.py
@@ -10,10 +10,10 @@ def init_db():
 
     if not os.path.exists(path):
         initial_data = {
-            "Media To Download": {
-                "Movies": [],
-                "Tv Shows": [],
-                "Music": []
+            "media to download": {
+                "movies": [],
+                "tvshows": [],
+                "music": []
             }
         }
         with open('downloads.json', 'w') as f:
@@ -29,7 +29,7 @@ def load_menu():
             return json.load(f)
     except FileNotFoundError:
         # If the file is missing, return the empty skeleton
-        return {"Media To Download": {"Movies": [], "Tv Shows": [], "Music": []}}
+        return {"media to download": {"movies": [], "tvshows": [], "music": []}}
 
 
 def save_menu(data):
@@ -44,26 +44,26 @@ def save_menu(data):
         print(f"An unexpected error occured: {e}")
 
 def add_item(data, category, name, year, downloaded=False, seasons=None, album=None):
-    clean_category = category.strip().title()
+    clean_category = category.strip().lower().replace(" ", "")
     
-    if clean_category not in data["Media To Download"]:
+    if clean_category not in data["media to download"]:
         print(f"Error: '{clean_category}' is not a valid category.")
         return
 
-    if seasons and clean_category != "Tv Shows":
+    if seasons and clean_category != "tvshows":
         print(f"You cannot add Seasons to the '{clean_category}' category.")
         return
     
-    if album and clean_category != "Music":
+    if album and clean_category != "music":
         print(f"You cannot add Album to the '{clean_category}' category.")
         return
 
-    menu_category = data["Media To Download"][clean_category]
+    menu_category = data["media to download"][clean_category]
 
     # check if item exists first
     existing_item = None
     for item in menu_category:
-        if item["Name"].lower() == name.lower():
+        if item["name"].lower() == name.lower():
             existing_item = item
             break
     
@@ -71,38 +71,46 @@ def add_item(data, category, name, year, downloaded=False, seasons=None, album=N
     if existing_item:
         if seasons:
             # check if key exists
-            if "Seasons" not in existing_item:
-                existing_item["Seasons"] = {}
+            if "seasons" not in existing_item:
+                existing_item["seasons"] = {}
             
             # merge new season with existing ones
-            existing_item["Seasons"].update(seasons)
+            existing_item["seasons"].update(seasons)
             print(f"Updated seasons for {name}.")
 
         if album:
             # check if key exists
-            if "Album" not in existing_item:
-                existing_item["Albums"] = {}
+            if "album" not in existing_item:
+                existing_item["album"] = {}
             
             # add the new album and its status
-            existing_item["Album"].update({album: downloaded})
+            existing_item["album"].update(album)
             print(f"Updated album list for {name}.")
 
     # create item if it doesn't exist
     else:
         new_data = {
-            "Name": name,
-            "Year": year,
-            "Downloaded": downloaded
+            "name": name
         }
+
+        # only add the top-level 'year' if it's not music
+        if clean_category != "music":
+            new_data["year"] = year
+        
+        # only add 'downloaded' if it's a movie
+        if clean_category == "movies":
+            new_data["downloaded"] = downloaded
 
         # check for option TV info
         if seasons:
-            new_data["Seasons"] = seasons
+            new_data["seasons"] = seasons
 
         if album:
-            new_data["Album"] = {album: downloaded}
+            new_data["album"] = album
 
-    menu_category.append(new_data)
-    menu_category.sort(key=lambda x: x["Name"].lower())
+        menu_category.append(new_data)
+        print(f"Added new entry: {name}")
+
+    menu_category.sort(key=lambda x: x["name"].lower())
     save_menu(data)
 
