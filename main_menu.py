8377e12a7f6e33116d756a49f3bc4dd89a96acb3
VirgilsPath <132574864+VirgilsPath@users.noreply.github.com>
2026-04-02 21:39:19 -0500

    Currently still making a main menu that will have options to show a specific list, add or delete item, and let the user know the list is empty.

--- main_menu.py
+++ main_menu.py
@@ -3,4 +3,34 @@ import tools_tracker
 # tools_tracker.init_db()
 
 data = tools_tracker.load_menu()
-print(data)
+
+while True:
+    print("----- Main Menu -----")
+    print("1. Show Movie List")
+    """print("2. Show TV Show List")
+    print("3. Show Music List")"""
+    print("4. Exit")
+    print()
+
+    choice = input("Choose a number to proceed: ")
+
+    if choice == "1":
+        # find list
+        movie_list = data["Media To Download"]["Movies"]
+        
+        # check if list has movies
+        if len(movie_list) > 0:
+            print("----- Movies -----")
+            for mov in movie_list:
+                print(f"{mov["Name"]}: {mov["Year"]}: {mov["Downloaded"]}")
+        else:
+            # list is empty
+            print("\nYour movie list is empty.")
+
+        user_input = input("Type 'back' to go back or 'exit' to exit: ")
+
+        if user_input.lower() == 'back':
+            continue
+        elif user_input.lower() == 'exit':
+            print("Goodbye!")
+            break
--- tools_tracker.py
+++ tools_tracker.py
@@ -31,6 +31,32 @@ def load_menu():
         # If the file is missing, return the empty skeleton
         return {"Media To Download": {"Movies": [], "TV Shows": [], "Music": []}}
 
-# need to make a load function
-# need to make a save function
-# need to make a add_item function
+
+def save_menu(data):
+    path = get_path()
+    try:
+        with open(path, 'w') as f:
+            json.dump(data, f, indent=4)
+            print("Data saved successfully!")
+    except TypeError:
+        print("Error: The data you provided isn't compatible with JSON format.")
+    except Exception as e:
+        print(f"An unexpected error occured: {e}")
+
+def add_item(data, category, name, year, downloaded=False):
+    clean_category = category.strip().title()
+    if clean_category not in data["Media To Download"]:
+        print(f"Error: '{clean_category}' is not a valid category.")
+        return
+
+    menu_category = data["Media To Download"][clean_category]
+
+    new_data = {
+        "Name": name,
+        "Year": year,
+        "Downloaded": downloaded
+    }
+    menu_category.append(new_data)
+    menu_category.sort(key=lambda x: x["Name"].lower())
+    save_menu(data)
+
